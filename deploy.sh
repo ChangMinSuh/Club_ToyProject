b6852ee6d80736a8de598d957c21a0307624faa8
ChangMinSuh <timssuh@naver.com>
2022-02-15 13:57:37 +0900

    deploy.sh cd 삭제

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-docker pull timssuh/sweetclub-back
-docker pull timssuh/sweetclub-front
-docker pull timssuh/sweetclub-nginx
-
-cd /app/deploy/SweetClub
+# docker pull timssuh/sweetclub-back
+# docker pull timssuh/sweetclub-front
+# docker pull timssuh/sweetclub-nginx
 docker-compose up
 # 여기 수정
