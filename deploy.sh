867595e16d760d67ba48e517547c344ee1e73fb4
ChangMinSuh <timssuh@naver.com>
2022-06-01 02:28:30 +0900

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 cd /home/ubuntu/deploy/SweetClub
+docker rm -f $(docker ps -aq)
+docker rmi $(docker images -q)
+
+docker pull timssuh/sweetclub_back
+docker pull timssuh/sweetclub-front
+docker pull timssuh/sweetclub-nginx
+
 docker-compose up
